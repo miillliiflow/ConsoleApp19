532709fde308bfc384e3547cb12dbb3d242dfe63
miillliiflow <am555333999@gmail.com>
2024-02-20 14:59:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1) Выполнить инициализацию одномерного массива с использованием конструктора без
+            //указания значений элементов, заполнить массив с клавиатуры и вывести на экран.
+
+            //int i;
+            //int[] omas = new int[6];
+            //for (i = 0; i < omas.Length; i++)
+            //{
+            //    Console.Write($"omas[{i}]= ");
+            //    omas[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (i = 0; i < 6; i++)
+            //{
+            //    Console.WriteLine($"omas[{i}]= {omas[i]}");
+            //}
+
+            //2) Инициализировать одномерный массив с использованием конструктора , без указания размерности массива и без клавиатуры
+
+            //int i;
+            //int[] omas = { -3, 5, 7, -1, 10, 15 };
+            //for (i = 0; i < omas.Length; i++)
+            //{
+            //    Console.Write($"omas[{i}]= ");
+            //    omas[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (i = 0; i < omas.Length; i++)
+            //{
+            //    Console.WriteLine($"omas[{i}]= {omas[i]}");
+            //}
+
+            //3) Заполнить одномерный массив соответствующими индексами (по формуле) и вывести с использованием цикла foreach.
+
+            //int[] omas = new int[5];
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    //Console.WriteLine($"omas[{i}] = ");
+            //    omas[i] = i;
+            //}
+            //foreach (var element in omas)
+            //{
+            //    Console.WriteLine($"{element}\t ");
+            //}
+
+            //4) Заполнить одномерный массив случайным образом и вывести на экран с использованием цикла foreach.
+
+            //Random rnd = new Random();
+            //int[] omas = new int[5];
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    omas[i] = rnd.Next(-10,10);
+            //}
+            //foreach (var element in omas)
+            //{
+            //    Console.WriteLine($"{element}\t ");
+            //}
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
